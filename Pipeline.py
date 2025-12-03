1c79b84547c3812a20cecd15be66c13f86034e0c
ocxacell <lemussebastian843@gmail.com>
2025-12-03 16:16:13 -0500

    Se realizo el segundo intento de probar la funcion para limitar el tiempo minimo y maximo del target

--- Pipeline.py
+++ Pipeline.py
@@ -1,8 +1,11 @@
 from Lectura.Lectura_excel import leer_excel
-from Preprocesado.ModificacionBaseDeDatos import Limitar_tiempos
-from Utilidad.Conversion_fechas import DateToData
+from Preprocesado.ModifcacionBaseDeDatos import Limitar_tiempos
+from Utilidad.Conversion_fechas import Date_To_Data
 import pandas as pd
 if __name__ == "__main__":
     df = leer_excel(r'C:\Users\lemus\Documents\TESISAZTECA\Codigo\Datos\Datosrecorte_limpieza_de_motivosextra.xlsx')
-    df = DateToData(df,"Fecha/Hora de apertura")
+    df = Date_To_Data(df,"Fecha/Hora de apertura")
+    LimiteSuperior=40
+    LimiteInferior=1
+    Limitar_tiempos(LimiteSuperior,LimiteInferior)
     
