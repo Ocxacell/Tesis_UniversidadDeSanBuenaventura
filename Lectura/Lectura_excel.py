8bc04a183a9c4a04f099b03a0865af7492f9b983
ocxacell <lemussebastian843@gmail.com>
2025-12-03 16:34:00 -0500

    Se quitaron los prints de la funcion leer_excel que en un inicio sirvieron para validar la correcta extraccion

--- Lectura/Lectura_excel.py
+++ Lectura/Lectura_excel.py
@@ -4,6 +4,4 @@ def leer_excel(archivo_excel: str):
     ###############Lectura del excel###############
     df = pd.read_excel(archivo_excel)
     print("Datos cargados correctamente")
-    print("Vista previa de los datos:")
-    print(df.head())
     return df
