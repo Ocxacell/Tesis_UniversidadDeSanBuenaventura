0f757beeb93e24d0a844a163fe92da7bb1be6932
ocxacell <lemussebastian843@gmail.com>
2025-12-03 16:32:51 -0500

    Se corrigio el funcionamiento de Limitar_tiempos, faltaba ingresar el df a la funcion. Ademas se agrego una funcion para lidiar con valores vacios, elimina las columnas con demasiados vacios y rellena con _Desconocido_ las que tengan una cantidad adecuada(Elegida por el usuario)

--- Preprocesado/ModifcacionBaseDeDatos.py
+++ Preprocesado/ModifcacionBaseDeDatos.py
@@ -1,5 +1,19 @@
 import pandas as pd
 
-def Limitar_tiempos(RangoSuperior, RangoInferior):
+def Limitar_tiempos(df,RangoSuperior, RangoInferior):
     df = df[df["tiempo_solucion_horas"] >= RangoInferior]
     df = df.drop(df[df["tiempo_solucion_horas"] > RangoSuperior].index)
+    return df
+
+def Arreglar_vacios(df, NanAdmitidos):
+    col_names = df.columns.tolist()
+    for col in col_names:
+        print("Distruciion de la columna:", col)
+        print("Columnas nulas")
+        vacias=df[col].isnull().sum()
+        print(vacias)
+        if vacias < 1000:
+            df = df.dropna(subset=[col])
+        else:
+            df[col] = df[col].fillna('Desconocido')
+    return df
