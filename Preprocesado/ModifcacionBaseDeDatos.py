e8944f8316f0f30578814d13d057bdc378a879cb
ocxacell <lemussebastian843@gmail.com>
2025-12-04 10:39:36 -0500

    Arreglo de la funcion Arreglar_vacios

--- Preprocesado/ModifcacionBaseDeDatos.py
+++ Preprocesado/ModifcacionBaseDeDatos.py
@@ -5,14 +5,15 @@ def Limitar_tiempos(df,RangoSuperior, RangoInferior):
     df = df.drop(df[df["tiempo_solucion_horas"] > RangoSuperior].index)
     return df
 
-def Arreglar_vacios(df, NanAdmitidos):
+def Arreglar_vacios(df, NanAdmitidos=1000):
+    print("Limpieza de features")
     col_names = df.columns.tolist()
     for col in col_names:
-        print("Distruciion de la columna:", col)
+        print("Distrubucion de la columna:", col)
         print("Columnas nulas")
         vacias=df[col].isnull().sum()
         print(vacias)
-        if vacias < 1000:
+        if vacias < NanAdmitidos:
             df = df.dropna(subset=[col])
         else:
             df[col] = df[col].fillna('Desconocido')
