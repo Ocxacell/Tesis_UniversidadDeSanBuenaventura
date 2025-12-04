f693ba884336469a8fa3c3b72ac396e608cde9a1
ocxacell <lemussebastian843@gmail.com>
2025-12-04 10:47:54 -0500

    Integracion de la funcion para quitar vacios

--- Pipeline.py
+++ Pipeline.py
@@ -1,6 +1,6 @@
 from Lectura.Lectura_excel import leer_excel
-from Preprocesado.ModifcacionBaseDeDatos import Limitar_tiempos
-from Utilidad.Conversion_fechas import Date_To_Data
+from Preprocesado.ModifcacionBaseDeDatos import Limitar_tiempos, Arreglar_vacios
+from Utilidad.Conversion_fechas import Date_To_Data 
 import pandas as pd
 if __name__ == "__main__":
     df = leer_excel(r'C:\Users\lemus\Documents\TESISAZTECA\Codigo\Datos\Datosrecorte_limpieza_de_motivosextra.xlsx')
@@ -8,4 +8,7 @@ if __name__ == "__main__":
     LimiteSuperior=40
     LimiteInferior=1
     Limitar_tiempos(df,LimiteSuperior,LimiteInferior)
-
+    Arreglar_vacios(df,14000)
+    ColumnasIgnoradas = ['Fecha/Hora de apertura','tiempo_solucion_horas','Fecha_Solucion','tiempo_solucion_log']
+    X=df.drop(columns=ColumnasIgnoradas)
+    
