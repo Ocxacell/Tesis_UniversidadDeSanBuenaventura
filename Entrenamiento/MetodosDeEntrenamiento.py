ebca92b7fb07197517fa232d00e4eb94d874eb4d
ocxacell <lemussebastian843@gmail.com>
2025-12-04 15:58:53 -0500

    Se creo preparo el algoritmo de entrenamiento mediante el uso de la libreria CatBoost

---
+++ Entrenamiento/MetodosDeEntrenamiento.py
@@ -0,0 +1,28 @@
+import pandas as pd
+from catboost import CatBoostRegressor
+from sklearn.model_selection import train_test_split
+
+def Entrenamiento_cat (X,y,ite=1000,Lr=0.05,Ls="MAE",Depth=5,Verbose=200,Seed=15,EarlyStop=200):
+    X_train, X_test, y_train_log, y_test_log = train_test_split(X, y, test_size=0.2, random_state=15)
+    # ⿨ Identificar features categóricas y numéricas
+    cat_features = X_train.select_dtypes(include=["object"]).columns.tolist()
+    model = CatBoostRegressor(
+    iterations=ite,
+    learning_rate=Lr,
+    depth=Depth,
+    loss_function=Ls,
+    cat_features=cat_features,
+    eval_metric=Ls,
+    verbose=Verbose,
+    random_seed=Seed,
+    early_stopping_rounds=EarlyStop
+    )
+    model.fit(
+    X_train, y_train_log,
+    cat_features=cat_features,
+    eval_set=(X_test, y_test_log),
+    use_best_model=True,
+    plot=False
+    )
+    return model
+    
