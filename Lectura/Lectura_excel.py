d48cf703ea1b2d66f36ec12693dd096a0ee8d58d
ocxacell <lemussebastian843@gmail.com>
2025-12-08 23:50:49 -0500

    Cambio de nombre del script

--- Lectura/Lectura_excel.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-def leer_excel(archivo_excel: str):
-    ###############Lectura del excel###############
-    df = pd.read_excel(archivo_excel)
-    print("Datos cargados correctamente")
-    return df
