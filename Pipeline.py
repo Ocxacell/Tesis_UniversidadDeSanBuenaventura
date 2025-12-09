44dfbbb0afd67733820359fa0b336411ec82dba7
ocxacell <lemussebastian843@gmail.com>
2025-12-08 23:57:20 -0500

    Se hicieron pruebas de entrenamiento y se grafico la funcion de perdidas del modelo por primera vez

--- Pipeline.py
+++ Pipeline.py
@@ -1,10 +1,12 @@
-from Lectura.Lectura_excel import leer_excel
+from Lectura.LecturaExcel import Leer_excel
 from Preprocesado.ModifcacionBaseDeDatos import Limitar_tiempos, Arreglar_vacios
-from Utilidad.Conversion_fechas import Date_To_Data 
+from Utilidad.ConversionFechas import Date_To_Data 
 from Entrenamiento.MetodosDeEntrenamiento import Entrenamiento_cat
+from Graficos.GeneracionDeGraficas import Graficacion_loss
 import pandas as pd
+
 if __name__ == "__main__":
-    df = leer_excel(r'C:\Users\lemus\Documents\TESISAZTECA\Codigo\Datos\Datosrecorte_limpieza_de_motivosextra.xlsx')
+    df = Leer_excel(r'C:\Users\lemus\Documents\TESISAZTECA\Codigo\Datos\Datosrecorte_limpieza_de_motivosextra.xlsx')
     df = Date_To_Data(df,"Fecha/Hora de apertura")
     LimiteSuperior=40
     LimiteInferior=1
@@ -13,6 +15,6 @@ if __name__ == "__main__":
     ColumnasIgnoradas = ['Fecha/Hora de apertura','tiempo_solucion_horas','Fecha_Solucion']
     X=df.drop(columns=ColumnasIgnoradas)
     y=df['tiempo_solucion_horas']
-    print(X)
-    print(y)
-    Entrenamiento_cat(X,y,ite=3000)
+    Ls="MAE"
+    modelo=Entrenamiento_cat(X,y,ite=500,Depth=8,Ls=Ls)  
+    Graficacion_loss(modelo,Ls)
